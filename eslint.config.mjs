affa28d0bf29200313d8bd62be3d7a276717a16a
mbaula <markbaula.1@gmail.com>
2024-12-23 10:35:26 -0500

    remove lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "off",
+      "react/no-unescaped-entities": "off",
+      "no-unused-vars": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
